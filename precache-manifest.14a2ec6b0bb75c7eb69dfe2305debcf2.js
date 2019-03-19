75fff08192c8b1f8622ed1fd0726e46dc0b64302
Sum Guy <gjserq@gmail.com>
2019-03-18 20:56:53 -0700

    Updates

---
+++ precache-manifest.14a2ec6b0bb75c7eb69dfe2305debcf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda0924bd23e3d9029fa",
+    "url": "/Clicky-game/static/js/runtime~main.952307d2.js"
+  },
+  {
+    "revision": "e5e795b9bad951d6ab6a",
+    "url": "/Clicky-game/static/js/main.3fc72b78.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/Clicky-game/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "e5e795b9bad951d6ab6a",
+    "url": "/Clicky-game/static/css/main.b52f54ad.chunk.css"
+  },
+  {
+    "revision": "76d74444cdfdc8c9b1132c7f096f0454",
+    "url": "/Clicky-game/index.html"
+  }
+];
